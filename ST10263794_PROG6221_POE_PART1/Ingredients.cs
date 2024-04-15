0a67ee99d112e0a53cf936a18f5a34fa31095e48
ST10263794 <ST10263794@vcconnect.edu.za>
2024-04-15 19:36:55 +0200

    Ingredients class created to store get and set methods for the characteristics of the ingredients.

---
+++ ST10263794_PROG6221_POE_PART1/Ingredients.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10263794_PROG6221_POE_PART1
+{
+    internal class Ingredients
+    {
+     
+        public string name {  get; set; }
+        public string unit { get; set; }
+        public double quantity {  get; set; }
+
+    }
+}
