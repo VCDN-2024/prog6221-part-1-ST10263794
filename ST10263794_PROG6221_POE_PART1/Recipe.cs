a12b768b288f3f5706e433d68dd6bf2add731bb1
ST10263794 <ST10263794@vcconnect.edu.za>
2024-04-15 19:51:26 +0200

    Recipe class added to store get and set methods for characteristics of the recipes.

---
+++ ST10263794_PROG6221_POE_PART1/Recipe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10263794_PROG6221_POE_PART1
+{
+    internal class Recipe
+    {
+        public string name {  get; set; }
+        public string[] Steps { get; set; }
+        public Ingredients[] Ingredients { get; set; }
+
+    }
+}
