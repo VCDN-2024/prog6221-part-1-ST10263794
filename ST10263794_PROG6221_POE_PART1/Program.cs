8d70756607b8fa7709faf64593c744b0c056ab7d
ST10263794 <ST10263794@vcconnect.edu.za>
2024-04-16 00:04:42 +0200

    Implemented a while loop in my main program class to prompt the user to select an option.

--- ST10263794_PROG6221_POE_PART1/Program.cs
+++ ST10263794_PROG6221_POE_PART1/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using ST10263794_PROG6221_POE_PART1;
+
+static void Main(string[] args)
+{
+    MyRecipeApp app = new MyRecipeApp();
+    bool running = true;
+
+    while (running)
+    {
+        Console.WriteLine("1. Enter recipe details");
+        Console.WriteLine("2. Display recipe");
+        Console.WriteLine("3. Scale recipe");
+        Console.WriteLine("4. Reset quantities");
+        Console.WriteLine("5. Clear data");
+        Console.WriteLine("6. Exit");
+
+        Console.WriteLine("Select an option:");
+        int option = int.Parse(Console.ReadLine());
+    }
+}
